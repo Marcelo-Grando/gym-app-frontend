0b4d87aed28d2471b01ad8c097b4e6dfdf71d807
Marcelo-Grando <marcelogrando017@gmail.com>
2026-06-03 12:27:58 -0300

    feat: add reg to ServiceWorkerRegister

--- app/components/ServiceWorkerRegister.tsx
+++ app/components/ServiceWorkerRegister.tsx
@@ -6,7 +6,7 @@ export function ServiceWorkerRegister() {
     if ("serviceWorker" in navigator) {
       navigator.serviceWorker
         .register("/service-worker.js")
-        .then(() => console.log("SW registrado"))
+        .then((reg) => console.log("SW registrado: ", reg))
         .catch((err) => console.error("SW error:", err));
     }
   }, []);
